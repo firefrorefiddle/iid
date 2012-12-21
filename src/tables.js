b2a8b66d0ecd7dac1bd8fd67d7249e36afc3fadb
Mike <mikehartl17@gmail.com>
2012-12-21 11:00:03 +0100

    added support for hinzufuegen

--- src/tables.js
+++ src/tables.js
@@ -4,6 +4,26 @@ function init_table(topElemName, name)
     document.getElementById(topElemName).innerHTML = table_basic_layout(name).html();
 }
 
+function validate_input(datum, waehrung, betrag, beschreibung, kategorie, person)
+{
+
+    if(betrag == "") 
+    {
+	alert("Bitte geben Sie einen Betrag ein!");
+	return false;
+    }
+
+    num = parseFloat(betrag);
+
+    if(num <= 0 || isNaN(num))
+    {
+	alert(betrag + " ist keine positive Zahl!");
+	return false;
+    }
+
+    return true;
+}
+
 function table_insert_row(name, 
 			  datum, 
 			  waehrung,
@@ -12,11 +32,14 @@ function table_insert_row(name,
 			  kategorie,
 			  person)
 {
+    if(!validate_input(datum, waehrung, betrag, beschreibung, kategorie, person))
+	return;
+
     newRow = $("\
 <tr>\
   <td>"+datum+"</td>\
   <td>"+waehrung+"</td>\
-  <td>"+betrag+"</td>\
+  <td>"+parseFloat(betrag).toFixed(2)+"</td>\
   <td>"+beschreibung+"</td>\
   <td>"+kategorie+"</td>\
   <td>"+person+"</td>\
@@ -60,13 +83,13 @@ function table_basic_layout(name)
 <tr id='"+name+"-inputRow'>\
   <form>\
     <td>\
-      <div class='input-append date' id='dp3' data-date='12-02-2012' data-date-format='dd-mm-yyyy'>\
-        <input class='span2' size='16' type='text' value='12-02-2012' readonly>\
+      <div class='input-append date' data-date='12-02-2012' data-date-format='dd-mm-yyyy'>\
+        <input class='span2' id='"+name+"-add-date' size='16' type='text' value='12-02-2012' readonly>\
         <span class='add-on'><i class='icon-calendar'></i></span>\
       </div>\
     </td>\
     <td>\
-      <select name='sel' class='span1' id='sel'>\
+      <select name='sel' class='span1' id='"+name+"-add-waehrung'>\
         <option value='EUR'>EUR</option>\
         <option value='USD'>USD</option>\
         <option value='GBP'>GBP</option>\
@@ -74,27 +97,35 @@ function table_basic_layout(name)
       </select>\
     </td>\
     <td>\
-      <input type='text' class='span1'></input>\
+      <input type='text' class='span1' id='"+name+"-add-betrag'></input>\
     </td>\
     <td>\
-      <input type='text' class='span1'></input>\
+      <input type='text' class='span1' id='"+name+"-add-beschreibung'></input>\
     </td>\
     <td>\
-      <select name='sel' class='span1' id='sel'>\
+      <select name='sel' class='span1' id='"+name+"-add-kategorie'>\
         <option value='Miete'>Miete</option>\
         <option value='Wohnen'>Wohnen</option>\
       </select>\
     </td>\
     <td>\
-      <select name='sel' class='span1' id='sel'>\
+      <select name='sel' class='span1' id='"+name+"-add-person'>\
         <option value='ich'>ich</option>\
         <option value='Selena'>Selena</option>\
       </select>\
     </td>\
-    <td/><td><a>Hinzufügen</a></td>\
+    <td/><td><a \
+             onClick='JavaScript:table_insert_row(\
+               \""+name+"\",\
+               $(\"#"+name+"-add-date\").val(),\
+               $(\"#"+name+"-add-waehrung\").val(),\
+               $(\"#"+name+"-add-betrag\").val(),\
+               $(\"#"+name+"-add-beschreibung\").val(),\
+               $(\"#"+name+"-add-kategorie\").val(),\
+               $(\"#"+name+"-add-person\").val()\
+             );'>Hinzufügen</a></td>\
   </form>\
 </tr>\
 </table>\
 </div>");
 }
-
